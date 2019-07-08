93e82186f34a7428b8fd02a5c2b0dbe2aa13fc4e
Lag <lagysera@gmail.com>
2019-07-08 18:28:38 +0200

    add World Events

--- AtlasLootClassic_Collections/data.lua
+++ AtlasLootClassic_Collections/data.lua
@@ -38,7 +38,7 @@ local PRICE_EXTRA_ITTYPE = data:AddExtraItemTableType("Price")
 local SET_EXTRA_ITTYPE = data:AddExtraItemTableType("Set")
 
 local SET_CONTENT = data:AddContentType(AL["Sets"], ATLASLOOT_PVP_COLOR)
-local GENERAL_CONTENT = data:AddContentType(GENERAL, ATLASLOOT_RAID40_COLOR)
+local WORLD_EVENT_CONTENT = data:AddContentType(AL["World Events"], ATLASLOOT_SEASONALEVENTS_COLOR)
 
 
 
@@ -107,4 +107,562 @@ data["TierSets"] = {
 		},
 	},
 
+}
+
+data["SilithusAbyssal"] = {
+	name = AL["Silithus Abyssal"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- AbyssalDukes
+			name = AL["Abyssal Dukes"],
+			[NORMAL_DIFF] = {
+				{ 1, "INV_Box_01", nil, AL["The Duke of Cynders"] },
+				{ 2,  20665 }, -- Abyssal Leather Leggings
+				{ 3,  20666 }, -- Hardened Steel Warhammer
+				{ 4,  20514 }, -- Abyssal Signet
+				{ 5,  20664 }, -- Abyssal Cloth Sash
+				{ 8, "INV_Box_01", nil, AL["The Duke of Fathoms"] },
+				{ 9,  20668 }, -- Abyssal Mail Legguards
+				{ 10, 20669 }, -- Darkstone Claymore
+				{ 11, 20514 }, -- Abyssal Signet
+				{ 12, 20667 }, -- Abyssal Leather Belt
+				{ 16, "INV_Box_01", nil, AL["The Duke of Zephyrs"] },
+				{ 17, 20674 }, -- Abyssal Cloth Pants
+				{ 18, 20675 }, -- Soulrender
+				{ 19, 20514 }, -- Abyssal Signet
+				{ 20, 20673 }, -- Abyssal Plate Girdle
+				{ 23, "INV_Box_01", nil, AL["The Duke of Shards"] },
+				{ 24, 20671 }, -- Abyssal Plate Legplates
+				{ 25, 20672 }, -- Sparkling Crystal Wand
+				{ 26, 20514 }, -- Abyssal Signet
+				{ 27, 20670 }, -- Abyssal Mail Clutch
+			},
+		},
+		{ -- AbyssalLords
+			name = AL["Abyssal Lords"],
+			[NORMAL_DIFF] = {
+				{ 1, "INV_Box_01", nil, AL["Prince Skaldrenox"] },
+				{ 2,  20682 }, -- Elemental Focus Band
+				{ 3,  20515 }, -- Abyssal Scepter
+				{ 4,  20681 }, -- Abyssal Leather Bracers
+				{ 5,  20680 }, -- Abyssal Mail Pauldrons
+				{ 7, "INV_Box_01", nil, AL["Lord Skwol"] },
+				{ 8,  20685 }, -- Wavefront Necklace
+				{ 9,  20515 }, -- Abyssal Scepter
+				{ 10, 20684 }, -- Abyssal Mail Armguards
+				{ 11, 20683 }, -- Abyssal Plate Epaulets
+				{ 16, "INV_Box_01", nil, AL["High Marshal Whirlaxis"] },
+				{ 17, 20691 }, -- Windshear Cape
+				{ 18, 20515 }, -- Abyssal Scepter
+				{ 19, 20690 }, -- Abyssal Cloth Wristbands
+				{ 20, 20689 }, -- Abyssal Leather Shoulders
+				{ 22, "INV_Box_01", nil, AL["Baron Kazum"] },
+				{ 23, 20688 }, -- Earthen Guard
+				{ 24, 20515 }, -- Abyssal Scepter
+				{ 25, 20686 }, -- Abyssal Cloth Amice
+				{ 26, 20687 }, -- Abyssal Plate Vambraces
+			},
+		},
+		{ -- AbyssalTemplars
+			name = AL["Abyssal Templars"],
+			[NORMAL_DIFF] = {
+				{ 1, "INV_Box_01", nil, AL["Crimson Templar"] },
+				{ 2,  20657 }, -- Crystal Tipped Stiletto
+				{ 3,  20655 }, -- Abyssal Cloth Handwraps
+				{ 4,  20656 }, -- Abyssal Mail Sabatons
+				{ 5,  20513 }, -- Abyssal Crest
+				{ 7, "INV_Box_01", nil, AL["Azure Templar"] },
+				{ 8,  20654 }, -- Amethyst War Staff
+				{ 9,  20653 }, -- Abyssal Plate Gauntlets
+				{ 10, 20652 }, -- Abyssal Cloth Slippers
+				{ 11, 20513 }, -- Abyssal Crest
+				{ 16, "INV_Box_01", nil, AL["Hoary Templar"] },
+				{ 17, 20660 }, -- Stonecutting Glaive
+				{ 18, 20659 }, -- Abyssal Mail Handguards
+				{ 19, 20658 }, -- Abyssal Leather Boots
+				{ 20, 20513 }, -- Abyssal Crest
+				{ 22, "INV_Box_01", nil, AL["Earthen Templar"] },
+				{ 23, 20663 }, -- Deep Strike Bow
+				{ 24, 20661 }, -- Abyssal Leather Gloves
+				{ 25, 20662 }, -- Abyssal Plate Greaves
+				{ 26, 20513 }, -- Abyssal Crest
+			},
+		},
+	},
+}
+
+data["ElementalInvasions"] = {
+	name = AL["Elemental Invasions"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- ElementalInvasion
+			name = AL["Elemental Invasions"],
+			[NORMAL_DIFF] = {
+				{ 1, "INV_Box_01", nil, AL["Baron Charr"] },
+				{ 2,  18671 }, -- Baron Charr's Sceptre
+				{ 3,  19268 }, -- Ace of Elementals
+				{ 4,  18672 }, -- Elemental Ember
+				{ 6, "INV_Box_01", nil, AL["Princess Tempestria"] },
+				{ 7,  18678 }, -- Tempestria's Frozen Necklace
+				{ 8,  19268 }, -- Ace of Elementals
+				{ 9,  21548 }, -- Pattern: Stormshroud Gloves
+				{ 10, 18679 }, -- Frigid Ring
+				{ 16, "INV_Box_01", nil, AL["Avalanchion"] },
+				{ 17, 18673 }, -- Avalanchion's Stony Hide
+				{ 18, 19268 }, -- Ace of Elementals
+				{ 19, 18674 }, -- Hardened Stone Band
+				{ 21, "INV_Box_01", nil, AL["The Windreaver"] },
+				{ 22, 18676 }, -- Sash of the Windreaver
+				{ 23, 19268 }, -- Ace of Elementals
+				{ 24, 21548 }, -- Pattern: Stormshroud Gloves
+				{ 25, 18677 }, -- Zephyr Cloak
+			},
+		},
+	},
+}
+
+data["GurubashiArena"] = {
+	name = AL["Gurubashi Arena"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- GurubashiArena
+			name = AL["Gurubashi Arena"],
+			[NORMAL_DIFF] = {
+				{ 1,  18709 }, -- Arena Wristguards
+				{ 2,  18710 }, -- Arena Bracers
+				{ 3,  18711 }, -- Arena Bands
+				{ 4,  18712 }, -- Arena Vambraces
+				{ 16, 18706 }, -- Arena Master
+				{ 17, 19024 }, -- Arena Grand Master
+			},
+		},
+	},
+}
+
+data["FishingExtravaganza"] = {
+	name = AL["Stranglethorn Fishing Extravaganza"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- FishingExtravaganza
+			name = AL["Stranglethorn Fishing Extravaganza"],
+			[NORMAL_DIFF] = {
+				{ 1, "INV_Box_01", nil, AL["First Prize"] },
+				{ 2,  19970 }, -- Arcanite Fishing Pole
+				{ 3,  19979 }, -- Hook of the Master Angler
+				{ 5, "INV_Box_01", nil, AL["Rare Fish"] },
+				{ 6,  19805 }, -- Keefer's Angelfish
+				{ 7,  19803 }, -- Brownell's Blue Striped Racer
+				{ 8,  19806 }, -- Dezian Queenfish
+				{ 9,  19808 }, -- Rockhide Strongfish
+				{ 20, "INV_Box_01", nil, AL["Rare Fish Rewards"] },
+				{ 21, 19972 }, -- Lucky Fishing Hat
+				{ 22, 19969 }, -- Nat Pagle's Extreme Anglin' Boots
+				{ 23, 19971 }, -- High Test Eternium Fishing Line
+			},
+		},
+	},
+}
+
+data["ChildrensWeek"] = {
+	name = AL["Childrens Week"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- ChildrensWeek
+			name = AL["Childrens Week"],
+			[NORMAL_DIFF] = {
+				{ 1,  23007 }, -- Piglet's Collar
+				{ 2,  23015 }, -- Rat Cage
+				{ 3,  23002 }, -- Turtle Box
+				{ 4,  23022 }, -- Curmudgeon's Payoff
+			},
+		},
+	},
+}
+
+data["Valentineday"] = {
+	name = AL["Valentineday"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- Valentineday
+			name = AL["Valentineday"],
+			[NORMAL_DIFF] = {
+				{ 1,  22206 }, -- Bouquet of Red Roses
+				{ 3, "INV_ValentinesBoxOfChocolates02", nil, AL["Gift of Adoration"] },
+				{ 4,  22279 }, -- Lovely Black Dress
+				{ 5,  22235 }, -- Truesilver Shafted Arrow
+				{ 6,  22200 }, -- Silver Shafted Arrow
+				{ 7,  22261 }, -- Love Fool
+				{ 8,  22218 }, -- Handful of Rose Petals
+				{ 9,  21813 }, -- Bag of Candies
+				{ 11, "INV_Box_02", nil, AL["Box of Chocolates"] },
+				{ 12, 22237 }, -- Dark Desire
+				{ 13, 22238 }, -- Very Berry Cream
+				{ 14, 22236 }, -- Buttermilk Delight
+				{ 15, 22239 }, -- Sweet Surprise
+				{ 16, 22276 }, -- Lovely Red Dress
+				{ 17, 22278 }, -- Lovely Blue Dress
+				{ 18, 22280 }, -- Lovely Purple Dress
+				{ 19, 22277 }, -- Red Dinner Suit
+				{ 20, 22281 }, -- Blue Dinner Suit
+				{ 21, 22282 }, -- Purple Dinner Suit
+			},
+		},
+	},
+}
+
+data["Halloween"] = {
+	name = AL["Hallow's End"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- Halloween1
+			name = AL["Hallow's End"].." - "..AL["Misc"],
+			[NORMAL_DIFF] = {
+				{ 1,  20400 }, -- Pumpkin Bag
+				{ 3,  18633 }, -- Styleen's Sour Suckerpop
+				{ 4,  18632 }, -- Moonbrook Riot Taffy
+				{ 5,  18635 }, -- Bellara's Nutterbar
+				{ 6,  20557 }, -- Hallow's End Pumpkin Treat
+				{ 8,  20389 }, -- Candy Corn
+				{ 9,  20388 }, -- Lollipop
+				{ 10, 20390 }, -- Candy Bar
+			},
+		},
+		{ -- Halloween1
+			name = AL["Hallow's End"].." - "..AL["Wands"],
+			[NORMAL_DIFF] = {
+				{ 1, 20410 }, -- Hallowed Wand - Bat
+				{ 2, 20409 }, -- Hallowed Wand - Ghost
+				{ 3, 20399 }, -- Hallowed Wand - Leper Gnome
+				{ 4, 20398 }, -- Hallowed Wand - Ninja
+				{ 5, 20397 }, -- Hallowed Wand - Pirate
+				{ 6, 20413 }, -- Hallowed Wand - Random
+				{ 7, 20411 }, -- Hallowed Wand - Skeleton
+				{ 8, 20414 }, -- Hallowed Wand - Wisp
+			},
+		},
+		{ -- Halloween3
+			name = AL["Hallow's End"].." - "..AL["Masks"],
+			[NORMAL_DIFF] = {
+				{ 1,  20561 }, -- Flimsy Male Dwarf Mask
+				{ 2,  20391 }, -- Flimsy Male Gnome Mask
+				{ 3,  20566 }, -- Flimsy Male Human Mask
+				{ 4,  20564 }, -- Flimsy Male Nightelf Mask
+				{ 5,  20570 }, -- Flimsy Male Orc Mask
+				{ 6,  20572 }, -- Flimsy Male Tauren Mask
+				{ 7,  20568 }, -- Flimsy Male Troll Mask
+				{ 8,  20573 }, -- Flimsy Male Undead Mask
+				{ 16, 20562 }, -- Flimsy Female Dwarf Mask
+				{ 17, 20392 }, -- Flimsy Female Gnome Mask
+				{ 18, 20565 }, -- Flimsy Female Human Mask
+				{ 19, 20563 }, -- Flimsy Female Nightelf Mask
+				{ 20, 20569 }, -- Flimsy Female Orc Mask
+				{ 21, 20571 }, -- Flimsy Female Tauren Mask
+				{ 22, 20567 }, -- Flimsy Female Troll Mask
+				{ 23, 20574 }, -- Flimsy Female Undead Mask
+			},
+		},
+	},
+}
+
+data["WinterVeil"] = {
+	name = AL["Feast of Winter Veil"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- Winterviel1
+			name = AL["Misc"],
+			[NORMAL_DIFF] = {
+				{ 1,  21525 }, -- Green Winter Hat
+				{ 2,  21524 }, -- Red Winter Hat
+				{ 16,  17712 }, -- Winter Veil Disguise Kit
+				{ 17,  17202 }, -- Snowball
+				{ 18,  21212 }, -- Fresh Holly
+				{ 19,  21519 }, -- Mistletoe
+			},
+		},
+		{
+			name = AL["Gaily Wrapped Present"],
+			[NORMAL_DIFF] = {
+				{ 1, 21301 }, -- Green Helper Box
+				{ 2, 21308 }, -- Jingling Bell
+				{ 3, 21305 }, -- Red Helper Box
+				{ 4, 21309 }, -- Snowman Kit
+			},
+		},
+		{
+			name = AL["Festive Gift"],
+			[NORMAL_DIFF] = {
+				{ 1, 21328 }, -- Wand of Holiday Cheer
+			},
+		},
+		{
+			name = AL["Smokywood Pastures Special Gift"],
+			[NORMAL_DIFF] = {
+				{ 1, 17706 }, -- Plans: Edge of Winter
+				{ 2, 17725 }, -- Formula: Enchant Weapon - Winter's Might
+				{ 3, 17720 }, -- Schematic: Snowmaster 9000
+				{ 4, 17722 }, -- Pattern: Gloves of the Greatfather
+				{ 5, 17709 }, -- Recipe: Elixir of Frost Power
+				{ 6, 17724 }, -- Pattern: Green Holiday Shirt
+				{ 16, 21325 }, -- Mechanical Greench
+				{ 17, 21213 }, -- Preserved Holly
+			},
+		},
+		{
+			name = AL["Gently Shaken Gift"],
+			[NORMAL_DIFF] = {
+				{ 1, 21235 }, -- Winter Veil Roast
+				{ 2, 21241 }, -- Winter Veil Eggnog
+			},
+		},
+		{
+			name = AL["Smokywood Pastures"],
+			[NORMAL_DIFF] = {
+				{ 1,  17201 }, -- Recipe: Egg Nog
+				{ 2,  17200 }, -- Recipe: Gingerbread Cookie
+				{ 3,  17344 }, -- Candy Cane
+				{ 4,  17406 }, -- Holiday Cheesewheel
+				{ 5,  17407 }, -- Graccu's Homemade Meat Pie
+				{ 6,  17408 }, -- Spicy Beefstick
+				{ 7,  17404 }, -- Blended Bean Brew
+				{ 8,  17405 }, -- Green Garden Tea
+				{ 9, 17196 }, -- Holiday Spirits
+				{ 10, 17403 }, -- Steamwheedle Fizzy Spirits
+				{ 11, 17402 }, -- Greatfather's Winter Ale
+				{ 12, 17194 }, -- Holiday Spices
+				{ 16, 17303 }, -- Blue Ribboned Wrapping Paper
+				{ 17, 17304 }, -- Green Ribboned Wrapping Paper
+				{ 18, 17307 }, -- Purple Ribboned Wrapping Paper
+			},
+		},
+	},
+}
+
+data["Noblegarden"] = {
+	name = AL["Noblegarden"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- Noblegarden
+			name = AL["Brightly Colored Egg"],
+			[NORMAL_DIFF] = {
+				{ 1,  19028 }, -- Elegant Dress
+				{ 2,  6833 }, -- White Tuxedo Shirt
+				{ 3,  6835 }, -- Black Tuxedo Pants
+				{ 16,  7807 }, -- Candy Bar
+				{ 17,  7808 }, -- Chocolate Square
+				{ 18,  7806 }, -- Lollipop
+			},
+		},
+	},
+}
+
+data["HarvestFestival"] = {
+	name = AL["Harvest Festival"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- HarvestFestival
+			name = AL["Harvest Festival"],
+			[NORMAL_DIFF] = {
+				{ 1,  19697 }, -- Bounty of the Harvest
+				{ 2,  20009 }, -- For the Light!
+				{ 3,  20010 }, -- The Horde's Hellscream
+				{ 16,  19995 }, -- Harvest Boar
+				{ 17,  19996 }, -- Harvest Fish
+				{ 18,  19994 }, -- Harvest Fruit
+				{ 19,  19997 }, -- Harvest Nectar
+			},
+		},
+	},
+}
+
+data["ScourgeInvasion"] = {
+	name = AL["Scourge Invasion"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- ScourgeInvasionEvent1
+			name = AL["Scourge Invasion"],
+			[NORMAL_DIFF] = {
+				{ 1,  23123 }, -- Blessed Wizard Oil
+				{ 2,  23122 }, -- Consecrated Sharpening Stone
+				{ 3,  22999 }, -- Tabard of the Argent Dawn
+				{ 4,  22484 }, -- Necrotic Rune
+				{ 7,  23085 }, -- Robe of Undead Cleansing
+				{ 8,  23091 }, -- Bracers of Undead Cleansing
+				{ 9,  23084 }, -- Gloves of Undead Cleansing
+				{ 12, 23089 }, -- Tunic of Undead Slaying
+				{ 13, 23093 }, -- Wristwraps of Undead Slaying
+				{ 14, 23081 }, -- Handwraps of Undead Slaying
+				{ 16, 23194 }, -- Lesser Mark of the Dawn
+				{ 17, 23195 }, -- Mark of the Dawn
+				{ 18, 23196 }, -- Greater Mark of the Dawn
+				{ 22, 23088 }, -- Chestguard of Undead Slaying
+				{ 23, 23092 }, -- Wristguards of Undead Slaying
+				{ 24, 23082 }, -- Handguards of Undead Slaying
+				{ 27, 23087 }, -- Breastplate of Undead Slaying
+				{ 28, 23090 }, -- Bracers of Undead Slaying
+				{ 29, 23078 }, -- Gauntlets of Undead Slaying
+			},
+		},
+		{
+			name = AL["Stratholme"].." - "..AL["Balzaphon"],
+			[NORMAL_DIFF] = {
+				{ 1,  23126 }, -- Waistband of Balzaphon
+				{ 2,  23125 }, -- Chains of the Lich
+				{ 3,  23124 }, -- Staff of Balzaphon
+			}
+		},
+		{
+			name = AL["Scholomance"].." - "..AL["Lord Blackwood"],
+			[NORMAL_DIFF] = {
+				{ 1,  23132 }, -- Lord Blackwood's Blade
+				{ 2,  23156 }, -- Blackwood's Thigh
+				{ 3,  23139 }, -- Lord Blackwood's Buckler
+			}
+		},
+		{
+			name = AL["Dire Maul"].." - "..AL["Revanchion"],
+			[NORMAL_DIFF] = {
+				{ 1, 23127 }, -- Cloak of Revanchion
+				{ 2, 23129 }, -- Bracers of Mending
+				{ 3, 23128 }, -- The Shadow's Grasp
+			}
+		},
+		{
+			name = AL["Scarlet Monastery - Graveyard"].." - "..AL["Scorn"],
+			[NORMAL_DIFF] = {
+				{ 1, 23169 }, -- Scorn's Icy Choker
+				{ 2, 23170 }, -- The Frozen Clutch
+				{ 3, 23168 }, -- Scorn's Focal Dagger
+			}
+		},
+		{
+			name = AL["Shadowfang Keep"].." - "..AL["Sever"],
+			[NORMAL_DIFF] = {
+				{ 1, 23173 }, -- Abomination Skin Leggings
+				{ 2, 23171 }, -- The Axe of Severing
+			}
+		},
+		{
+			name = AL["Razorfen Downs"].." - "..AL["Lady Falther'ess"],
+			[NORMAL_DIFF] = {
+				{ 1, 23178 }, -- Mantle of Lady Falther'ess
+				{ 2, 23177 }, -- Lady Falther'ess' Finger
+			}
+		},
+	},
+}
+
+data["LunarFestival"] = {
+	name = AL["Lunar Festival"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- LunarFestival1
+			name = AL["Lunar Festival"],
+			[NORMAL_DIFF] = {
+				{ 1,  21100 }, -- Coin of Ancestry
+				{ 3,  21157 }, -- Festive Green Dress
+				{ 4,  21538 }, -- Festive Pink Dress
+				{ 5,  21539 }, -- Festive Purple Dress
+				{ 6,  21541 }, -- Festive Black Pant Suit
+				{ 7,  21544 }, -- Festive Blue Pant Suit
+				{ 8,  21543 }, -- Festive Teal Pant Suit
+			},
+		},
+		{
+			name = AL["Lunar Festival Fireworks Pack"],
+			[NORMAL_DIFF] = {
+				{ 1, 21558 }, -- Small Blue Rocket
+				{ 2, 21559 }, -- Small Green Rocket
+				{ 3, 21557 }, -- Small Red Rocket
+				{ 4, 21561 }, -- Small White Rocket
+				{ 5, 21562 }, -- Small Yellow Rocket
+				{ 7, 21537 }, -- Festival Dumplings
+				{ 8, 21713 }, -- Elune's Candle
+				{ 16, 21589 }, -- Large Blue Rocket
+				{ 17, 21590 }, -- Large Green Rocket
+				{ 18, 21592 }, -- Large Red Rocket
+				{ 19, 21593 }, -- Large White Rocket
+				{ 20, 21595 }, -- Large Yellow Rocket
+			}
+		},
+		{
+			name = AL["Lucky Red Envelope"],
+			[NORMAL_DIFF] = {
+				{ 1, 21540 }, -- Elune's Lantern
+				{ 2, 21536 }, -- Elune Stone
+				{ 16, 21744 }, -- Lucky Rocket Cluster
+				{ 17, 21745 }, -- Elder's Moonstone
+			}
+		},
+		{ -- LunarFestival2
+			name = AL["Plans"],
+			[NORMAL_DIFF] = {
+				{ 1,  21738 }, -- Schematic: Firework Launcher
+				{ 3,  21722 }, -- Pattern: Festival Dress
+				{ 4,  21154 }, -- Festival Dress
+				{ 7,  21724 }, -- Schematic: Small Blue Rocket
+				{ 8,  21725 }, -- Schematic: Small Green Rocket
+				{ 9,  21726 }, -- Schematic: Small Red Rocket
+				{ 12, 21727 }, -- Schematic: Large Blue Rocket
+				{ 13, 21728 }, -- Schematic: Large Green Rocket
+				{ 14, 21729 }, -- Schematic: Large Red Rocket
+				{ 16, 21737 }, -- Schematic: Cluster Launcher
+				{ 18, 21723 }, -- Pattern: Festive Red Pant Suit
+				{ 19, 21542 }, -- Festival Suit
+				{ 22, 21730 }, -- Schematic: Blue Rocket Cluster
+				{ 23, 21731 }, -- Schematic: Green Rocket Cluster
+				{ 24, 21732 }, -- Schematic: Red Rocket Cluster
+				{ 27, 21733 }, -- Schematic: Large Blue Rocket Cluster
+				{ 28, 21734 }, -- Schematic: Large Green Rocket Cluster
+				{ 29, 21735 }, -- Schematic: Large Red Rocket Cluster
+			},
+		},
+	},
+}
+
+data["MidsummerFestival"] = {
+	name = AL["Midsummer Festival"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	items = {
+		{ -- MidsummerFestival
+			name = AL["Midsummer Festival"],
+			[NORMAL_DIFF] = {
+				{ 1,  23379 }, -- Cinder Bracers
+				{ 3,  23323 }, -- Crown of the Fire Festival
+				{ 4,  23324 }, -- Mantle of the Fire Festival
+				{ 6,  23083 }, -- Captured Flame
+				{ 7,  23247 }, -- Burning Blossom
+				{ 8,  23246 }, -- Fiery Festival Brew
+				{ 9,  23435 }, -- Elderberry Pie
+				{ 10, 23327 }, -- Fire-toasted Bun
+				{ 11, 23326 }, -- Midsummer Sausage
+				{ 12, 23211 }, -- Toasted Smorc
+			},
+		},
+	},
 }
