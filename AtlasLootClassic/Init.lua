4aecd5f28694021681ee59cb709cbdd9fd4b93ad
Lag <lagysera@gmail.com>
2019-07-14 16:45:23 +0200

    add better versionscheck

--- AtlasLootClassic/Init.lua
+++ AtlasLootClassic/Init.lua
@@ -9,8 +9,26 @@ local tonumber = _G.tonumber
 -- ----------------------------------------------------------------------------
 local addonname = ...
 
+_G.AtlasLoot = { }
+
+local addonVersion = GetAddOnMetadata(addonname, "Version")
+if addonVersion == "@project-version@" then addonVersion = "v99.99.9999-dev" end
+local versionT = { string.match(addonVersion, "v(%d+)%.(%d+)%.(%d+)%-?(%w*)") }
+addonVersion = ""
+for k,v in ipairs(versionT) do
+	if k < 4 then
+		local it = k == 3 and (4 - #v) or (2 - #v)
+		for i = 1, it do
+			versionT[k] = "0"..versionT[k]
+		end
+		addonVersion = addonVersion..versionT[k]
+	end
+end
+
 _G.AtlasLoot = {
-	__addonrevision = tonumber(("$Rev: 4773 $"):match("%d+")) or 0
+	__addonrevision = tonumber(addonVersion),
+	IsDevVersion = versionT[4] == "dev" and true or nil,
+	IsTestVersion = (versionT[4] == "beta" or versionT[4] == "alpha") and true or nil,
 }
 
 local AddonNameVersion = string.format("%s-%d", addonname, _G.AtlasLoot.__addonrevision)
@@ -24,6 +42,7 @@ setmetatable(_G.AtlasLoot, MainMT)
 -- DB
 AtlasLootClassicDB = {}
 AtlasLootClassicCharDB = {}
+AtlasLootClassicRawDB = {}
 
 -- Translations
 _G.AtlasLoot.Locale = {}
