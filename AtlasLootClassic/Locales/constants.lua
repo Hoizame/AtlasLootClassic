3402d7d5dfef3b18d55e29c844e0e48b8afddda4
Lag <lagysera@gmail.com>
2019-07-17 22:38:41 +0200

    update Localization namespaces

--- AtlasLootClassic/Locales/constants.lua
+++ AtlasLootClassic/Locales/constants.lua
@@ -1,8 +1,3 @@
 ﻿local AL = _G.AtlasLoot.Locales
 -- These localization strings are translated on Curseforge: https://www.curseforge.com/wow/addons/atlaslootclassic/localization
---@localization(locale="enUS", true-if-value-equals-key = true, format="lua_additive_table", table-name="AL", handle-unlocalized="ignore", namespace="")@
---@do-not-package@
-
-
-
---@end-do-not-package@
+--@localization(locale="enUS", true-if-value-equals-key = true, format="lua_additive_table", table-name="AL", handle-unlocalized="ignore", namespace="AtlasLootClassic")@
