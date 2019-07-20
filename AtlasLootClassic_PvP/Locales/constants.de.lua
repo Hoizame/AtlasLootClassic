9a490077695e1363f3078a6cae3d32f42c5ad64c
Lag <lagysera@gmail.com>
2019-07-20 17:46:12 +0200

    locales

---
+++ AtlasLootClassic_PvP/Locales/constants.de.lua
@@ -0,0 +1,6 @@
+local AL = _G.AtlasLoot.GetLocales("deDE")
+
+if not AL then return end
+
+-- These localization strings are translated on Curseforge: https://www.curseforge.com/wow/addons/atlaslootclassic/localization
+--@localization(locale="deDE", format="lua_additive_table", table-name="AL", handle-unlocalized="ignore", namespace="PvP")@
