410e05503b5da076bea563f35e2ea8b77f716e90
Lag <lagysera@gmail.com>
2019-07-31 22:31:36 +0200

    some favourites option fixes

--- AtlasLootClassic_Options/Addons.lua
+++ AtlasLootClassic_Options/Addons.lua
@@ -10,6 +10,13 @@ local GetAddon = Addons.GetAddon
 
 local count = 0
 
+local function UpdateItemFrame(adoon, addonName)
+    adoon:UpdateStatus(addonName)
+    if AtlasLoot.GUI.frame and AtlasLoot.GUI.frame:IsShown() then
+        AtlasLoot.GUI.ItemFrame:Refresh(true)
+    end
+end
+
 local function CreateFavouriteOptions()
     count = count + 1
     local AddonName = "Favourites"
@@ -27,11 +34,45 @@ local function CreateFavouriteOptions()
                 name = _G.ENABLE,
                 set = function(info, value)
                     GetAddon(Addons, AddonName).db[info[#info]] = value
-                    Addons:UpdateStatus(AddonName)
-                    AtlasLoot.GUI.ItemFrame:Refresh(true)
+                    UpdateItemFrame(Addons, AddonName)
                 end
             },
-            
+            test = {
+                type = "select",
+                order = 2,
+                name = AL["List"],
+                values = function()
+                    local db = GetAddon(Addons, AddonName):GetDb()
+                    local list = {}
+                    if db.activeList[2] == true then --gloabal
+                        db = GetAddon(Addons, AddonName):GetGlobalDb()
+                    else
+                        db = db.lists
+                    end
+                    for k,v in pairs(db) do
+                        list[k] = k
+                    end
+                    return list
+                end,
+                get = function(info) return GetAddon(Addons, AddonName):GetDb().activeList[1] end,
+                set = function(info, value)
+                    GetAddon(Addons, AddonName):GetDb().activeList[1] = value
+                    UpdateItemFrame(Addons, AddonName)
+                end,
+            },
+            global = {
+                order = 3,
+                type = "toggle",
+                --width = "half",
+                name = AL["Use global list."],
+                get = function(info) return GetAddon(Addons, AddonName):GetDb().activeList[2] end,
+                set = function(info, value)
+                    local db = GetAddon(Addons, AddonName):GetDb()
+                    db.activeList[1] = "Base"
+                    db.activeList[2] = value
+                    UpdateItemFrame(Addons, AddonName)
+                end
+            },
         },
     }
     local args = t.args
