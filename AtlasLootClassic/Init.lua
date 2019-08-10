945168be18590ea7695dfab486ae61509033909a
Lag <lagysera@gmail.com>
2019-08-10 16:16:40 +0200

    fix display of version

--- AtlasLootClassic/Init.lua
+++ AtlasLootClassic/Init.lua
@@ -12,7 +12,7 @@ local addonname = ...
 _G.AtlasLoot = { }
 
 local addonVersion = GetAddOnMetadata(addonname, "Version")
-if addonVersion == "@project-version@" then addonVersion = "v99.99.9999-dev" end
+if addonVersion == string.format("@%s@", "project-version") then addonVersion = "v99.99.9999-dev" end
 local versionT = { string.match(addonVersion, "v(%d+)%.(%d+)%.(%d+)%-?(%a*)(%d*)") }
 local addonRevision = ""
 for k,v in ipairs(versionT) do
