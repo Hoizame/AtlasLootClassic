afe2b986ac087ed970a663e7b9613d7318e961f5
Lag <lagysera@gmail.com>
2019-08-20 20:33:12 +0200

    add Reagents addon basefile

---
+++ AtlasLootClassic/Addons/Reagents.lua
@@ -0,0 +1,41 @@
+local ALName, ALPrivate = ...
+
+local _G = getfenv(0)
+local AtlasLoot = _G.AtlasLoot
+local Addons = AtlasLoot.Addons
+local AL = AtlasLoot.Locales
+local Reagents = Addons:RegisterNewAddon("Reagents")
+local Tooltip = AtlasLoot.Tooltip
+
+-- lua
+
+
+-- WoW
+
+
+-- locals
+
+
+-- Addon
+Reagents.DbDefaults = {
+
+}
+
+--Reagents.GlobalDbDefaults = {}
+
+
+function Reagents.OnInitialize()
+
+end
+
+function Reagents:OnProfileChanged()
+
+end
+
+function Reagents:OnStatusChanged()
+
+end
+
+
+
+Reagents:Finalize()
