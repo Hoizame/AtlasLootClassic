c33237f0037a8e6e9ac0088415582ae5e47e1de6
Lag <lagysera@gmail.com>
2019-09-28 20:10:30 +0200

    auto select sub zone support

--- AtlasLootClassic/Data/AutoSelect.lua
+++ AtlasLootClassic/Data/AutoSelect.lua
@@ -6,11 +6,20 @@ AtlasLoot.Data.AutoSelect = AutoSelect
 local AL = AtlasLoot.Locales
 
 -- lua
+local unpack = unpack
 
 -- WoW
+local UnitPosition = UnitPosition
+local GetSubZoneText = GetSubZoneText
+
+local SUB_L = {}
+-- Locales data
+--##START-DATA##
+--##END-DATA##
 
 -- AutoSelect
 local AutoSelectSave = {}
+local LastBase
 
 function AutoSelect:RefreshOptions()
 	if AtlasLoot.db.enableAutoSelect and not AtlasLoot.Loader:IsModuleLoaded("AtlasLootClassic_DungeonsAndRaids") then
@@ -18,27 +27,51 @@ function AutoSelect:RefreshOptions()
 	end
 end
 
-function AutoSelect:Add(module, instanceAlID, instanceID)
-	if module and instanceAlID and instanceID then
-		AutoSelectSave[instanceID] = { module, instanceAlID }
-	end
-end
-
 function AutoSelect:AddInstanceTable(module, instanceAlID, iniTab)
     if not iniTab then return end
     local instanceID = iniTab.InstanceID
     if not module or not instanceAlID or not instanceID then return end
+    if not AutoSelectSave[instanceID] then
+        AutoSelectSave[instanceID] = {
+            base = { module, instanceAlID }
+        }
+    end
+    local content = AutoSelectSave[instanceID]
 
+    if iniTab.SubAreaIDs then
+        if not content.sub then
+            content.sub = {}
+            content.subList = {}
+        end
+
+        for i = 1, #iniTab.SubAreaIDs do
+            content.sub[iniTab.SubAreaIDs[i]] = { module, instanceAlID }
+            content.subList[#content.subList + 1] = iniTab.SubAreaIDs[i]
+        end
+        -- SubAreaIDs / SubAreaID
+        for i = 1, #iniTab.items do
+            local id = iniTab.items[i].SubAreaID
+            if id and content.sub[id] and not content.sub[id][3] then
+                content.sub[id][3] = id
+            end
+        end
+
+    end
 end
 
 function AutoSelect:GetCurrrentPlayerData()
-	local posY, posX, posZ, instanceID = UnitPosition("player")
-	if instanceID and AutoSelectSave[instanceID] then
-		if AutoSelectSave[instanceID][2][3] then
-			local bossID = nil
-			return AutoSelectSave[instanceID][1], AutoSelectSave[instanceID][2], bossID
-		else
-			return AutoSelectSave[instanceID][1], AutoSelectSave[instanceID][2]
-		end
+    local posY, posX, posZ, instanceID = UnitPosition("player")
+    local ini = instanceID and AutoSelectSave[instanceID] or nil
+    if ini then
+        if ini.sub then
+            local subZoneName = GetSubZoneText()
+            for i = 1, #ini.subList do
+                local locName = SUB_L[ini.subList[i]]
+                if locName and locName == subZoneName then
+                    return ini.base[1], ini.base[2], ini.sub[ini.subList[i]]
+                end
+            end
+        end
+        return unpack(ini.base)
 	end
 end
