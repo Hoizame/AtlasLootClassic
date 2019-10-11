eac69f58feaaf1ef719d06ddfaeec1bcbbb79d06
Lag <lagysera@gmail.com>
2019-10-11 23:52:55 +0200

    Sources code and data

--- AtlasLootClassic/Addons/Sources.lua
+++ AtlasLootClassic/Addons/Sources.lua
@@ -4,16 +4,19 @@ local _G = getfenv(0)
 local AtlasLoot = _G.AtlasLoot
 local Addons = AtlasLoot.Addons
 local AL = AtlasLoot.Locales
+local ALIL = AtlasLoot.Locales
 local Sources = Addons:RegisterNewAddon("Sources")
 local Tooltip = AtlasLoot.Tooltip
 
 -- lua
+local type = type
 local format = format
 
 -- WoW
 
 
 -- locals
+local TT_F = "%s |cFF00ccff%s|r"
 local DUMMY_ICON = "Interface\\Icons\\INV_Misc_QuestionMark"
 local TEXTURE_ICON_F, ATLAS_ICON_F = "|T%s:0|t ", "|A:%s:0:0|a "
 local ICON_TEXTURE = {
@@ -27,7 +30,7 @@ local ICON_TEXTURE = {
 	[7]  = format(TEXTURE_ICON_F, GetSpellTexture(2259)),   -- Alchemy
 	[8]  = format(TEXTURE_ICON_F, GetSpellTexture(2366)),   -- Herbalism
 	[9]  = format(TEXTURE_ICON_F, GetSpellTexture(2550)),   -- Cooking
-	[10]  = format(TEXTURE_ICON_F, GetSpellTexture(2575)),   -- Mining
+	[10] = format(TEXTURE_ICON_F, GetSpellTexture(2575)),   -- Mining
 	[11] = format(TEXTURE_ICON_F, GetSpellTexture(3908)),   -- Tailoring
 	[12] = format(TEXTURE_ICON_F, GetSpellTexture(4036)),   -- Engineering
 	[13] = format(TEXTURE_ICON_F, GetSpellTexture(7411)),   -- Enchanting
@@ -35,27 +38,120 @@ local ICON_TEXTURE = {
     [15] = format(TEXTURE_ICON_F, GetSpellTexture(8618)),   -- Skinning
     [16] = format(TEXTURE_ICON_F, GetSpellTexture(2842)),   -- Rogue: Poisons
 }
+local SOURCE_TYPES = {
+    [0]  = UNKNOWN,	                    -- UNKNOWN
+    [1]  = AL["Loot"],                  -- Loot
+    [2]  = AL["Quest"],                 -- Quest
+    [3]  = AL["Vendor"],                -- Buy
+	[4]  = ALIL["First Aid"],           -- First Aid
+	[5]  = ALIL["Blacksmithing"],       -- Blacksmithing
+	[6]  = ALIL["Leatherworking"],      -- Leatherworking
+	[7]  = ALIL["Alchemy"],             -- Alchemy
+	[8]  = ALIL["Herbalism"],           -- Herbalism
+	[9]  = ALIL["Cooking"],             -- Cooking
+	[10] = ALIL["Mining"],              -- Mining
+	[11] = ALIL["Tailoring"],           -- Tailoring
+	[12] = ALIL["Enchanting"],          -- Engineering
+	[13] = ALIL["Enchanting"],          -- Enchanting
+	[14] = ALIL["Fishing"],             -- Fishing
+    [15] = ALIL["Skinning"],            -- Skinning
+    [16] = ALIL["Poisons"],             -- Rogue: Poisons
+}
+local SOURCE_DATA
+local KEY_WEAK_MT = {__mode="k"}
+local AL_MODULE = "AtlasLootClassic_DungeonsAndRaids"
+
+local TooltipsHooked = false
+local TooltipCache, TooltipTextCache = {}
 
 -- Addon
 Sources.DbDefaults = {
-
+    enabled = true,
+    ["Sources"] = {
+        ["*"] = true,
+    }
 }
 
 --Sources.GlobalDbDefaults = {}
+local function BuildSource(ini, boss, typ)
+    if typ and typ > 3 then
+        -- Profession
+        return SOURCE_TYPES[typ]
+    end
+    if ini then
+        local iniName, bossName = AtlasLoot.ItemDB:GetNameData_UNSAFE(AL_MODULE, ini, boss)
+        if iniName and bossName then
+            return iniName.." - "..bossName
+        elseif iniName then
+            return iniName
+        end
+    end
+    return ""
+end
 
+local function OnTooltipSetItem_Hook(self)
+    if self:IsForbidden() or not SOURCE_DATA or not Sources.db.enabled then return end
+    local _, item = self:GetItem()
+    if not item then return end
+    if not TooltipCache[item] then
+        TooltipCache[item] = tonumber(strmatch(item, "item:(%d+)"))
+    end
+
+    item = TooltipCache[item]
+    if item and SOURCE_DATA.ItemData[item] then
+        if not TooltipTextCache[item] then
+            TooltipTextCache[item] = {}
+            if type(SOURCE_DATA.ItemData[item][1]) == "table" then
+                for i = 1, #SOURCE_DATA.ItemData[item] do
+                    local data = SOURCE_DATA.ItemData[item][i]
+                    TooltipTextCache[item][i] = format(TT_F, ICON_TEXTURE[data[3] or 0], BuildSource(SOURCE_DATA.AtlasLootIDs[data[1]],data[2],data[3]))
+                    self:AddLine(TooltipTextCache[item][i])
+                end
+            else
+                local data = SOURCE_DATA.ItemData[item]
+                TooltipTextCache[item][1] = format(TT_F, ICON_TEXTURE[data[3] or 0], BuildSource(SOURCE_DATA.AtlasLootIDs[data[1]],data[2],data[3]))
+                self:AddLine(TooltipTextCache[item][1])
+            end
+        else
+            for i = 1, #TooltipTextCache[item] do
+                self:AddLine(TooltipTextCache[item][i])
+            end
+        end
+    end
+end
+
+local function InitTooltips()
+    if TooltipsHooked then return end
+    Tooltip:AddHookFunction("OnTooltipSetItem", OnTooltipSetItem_Hook)
+    TooltipsHooked = true
+end
+
+function Sources:UpdateDb()
+    self.db = self:GetDb()
+
+    TooltipTextCache = {}
+    setmetatable(TooltipTextCache, KEY_WEAK_MT)
+
+    if self.db.enabled then
+        AtlasLoot.Loader:LoadModule("AtlasLootClassic_Data", InitTooltips)
+    end
+end
 
 function Sources.OnInitialize()
-
+    Sources:UpdateDb()
 end
 
 function Sources:OnProfileChanged()
-
+    Sources:UpdateDb()
 end
 
 function Sources:OnStatusChanged()
-
+    Sources:UpdateDb()
 end
 
-
+function Sources:SetData(dataTable)
+    if SOURCE_DATA then return end
+    SOURCE_DATA = dataTable
+end
 
 Sources:Finalize()
