b67b38c1cd903d75c4b37c1e441ec5b592ed4ceb
Lag <lagysera@gmail.com>
2019-12-01 17:46:54 +0100

    add Requirements.lua

---
+++ AtlasLootClassic/Data/Requirements.lua
@@ -0,0 +1,8 @@
+local AtlasLoot = _G.AtlasLoot
+local Requirements = {}
+AtlasLoot.Data.Requirements = Requirements
+local AL = AtlasLoot.Locales
+
+--##START-DATA##
+local ReqData = {}
+--##END-DATA##
