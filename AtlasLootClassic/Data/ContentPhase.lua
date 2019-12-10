d8b06a5034803c10995feabd4cb4a85e95e60271
Lag <lagysera@gmail.com>
2019-12-10 20:54:14 +0100

    set contentPhase 2.5

--- AtlasLootClassic/Data/ContentPhase.lua
+++ AtlasLootClassic/Data/ContentPhase.lua
@@ -5,7 +5,7 @@ local ContentPhase = {}
 AtlasLoot.Data.ContentPhase = ContentPhase
 local AL = AtlasLoot.Locales
 local IMAGE_PATH = ALPrivate.IMAGE_PATH
-local ACTIVE_PHASE = 2
+local ACTIVE_PHASE = 2.5
 
 --##START-DATA##
 local PHASE_ITEMS = {
