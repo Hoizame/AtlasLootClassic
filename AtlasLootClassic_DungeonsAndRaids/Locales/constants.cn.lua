28790bacf226fc4ba2789f1a1ab89a735c2f6bd0
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2020-06-04 19:34:55 +0800

    Update constants.cn.lua

--- AtlasLootClassic_DungeonsAndRaids/Locales/constants.cn.lua
+++ AtlasLootClassic_DungeonsAndRaids/Locales/constants.cn.lua
@@ -4,3 +4,9 @@ if not AL then return end
 
 -- These localization strings are translated on Curseforge: https://www.curseforge.com/wow/addons/atlaslootclassic/localization
 --@localization(locale="zhCN", format="lua_additive_table", table-name="AL", handle-unlocalized="ignore", namespace="DungeonsAndRaids")@
+
+
+
+AL["Vem"] = "亚尔基公主"
+AL["Princess Yauj"] = "维姆"
+AL["Lord Kri"] = "克里勋爵"
