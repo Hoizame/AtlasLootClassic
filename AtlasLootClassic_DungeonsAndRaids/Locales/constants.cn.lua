bd50f523cd0860cec590f1e0909c86c8f49439b7
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2020-11-20 19:46:46 +0800

    Update constants.cn.lua

--- AtlasLootClassic_DungeonsAndRaids/Locales/constants.cn.lua
+++ AtlasLootClassic_DungeonsAndRaids/Locales/constants.cn.lua
@@ -4,3 +4,4 @@ if not AL then return end
 
 -- These localization strings are translated on Curseforge: https://www.curseforge.com/wow/addons/atlaslootclassic/localization
 --@localization(locale="zhCN", format="lua_additive_table", table-name="AL", handle-unlocalized="ignore", namespace="DungeonsAndRaids")@
+AL["High Priest Shared loot"] = "高阶祭司共享掉落"
