b95a4a512c19a6eb7ff11a281f43142c1d39c451
Lag <lagysera@gmail.com>
2021-06-03 16:09:36 +0200

    update pvp mount itemIDs

--- AtlasLootClassic_PvP/data-tbc.lua
+++ AtlasLootClassic_PvP/data-tbc.lua
@@ -270,7 +270,6 @@ data["BCCReputationSet"] = {
 	},
 }
 
-local PVP_MOUNT_PRICE = "pvpAlterac:30:pvpArathi:30:pvpWarsong:30"
 data["PvPMountsBCC"] = {
 	name = ALIL["Mounts"],
 	ContentType = GENERAL_CONTENT,
@@ -279,24 +278,24 @@ data["PvPMountsBCC"] = {
 		{ -- PvPMountsPvP
 			name = ALIL["Mounts"],
 			[ALLIANCE_DIFF] = {
-				{ 1,  19030, [PRICE_EXTRA_ITTYPE] = "pvpAlterac:50" }, -- Stormpike Battle Charger
-				{ 3,  18244, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Black War Ram
-				{ 4,  18243, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Black Battlestrider
-				{ 5,  18241, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Black War Steed Bridle
-				{ 6,  18242, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Reins of the Black War Tiger
-				{ 7,  35906, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Reins of the Black War Elekk
+				{ 1,  19030 }, -- Stormpike Battle Charger
+				{ 3,  29467 }, -- Black War Ram
+				{ 4,  29465 }, -- Black Battlestrider
+				{ 5,  29468 }, -- Black War Steed Bridle
+				{ 6,  29471 }, -- Reins of the Black War Tiger
+				{ 7,  35906 }, -- Reins of the Black War Elekk
 				{ 16,  30609 }, -- Swift Nether Drake
 				{ 17,  37676 }, -- Vengeful Nether Drake
 				{ 18,  34092 }, -- Merciless Nether Drake
 				--{ 19,  43516 }, -- Brutal Nether Drake NYI
 			},
 			[HORDE_DIFF] = {
-				{ 1, 19029, [PRICE_EXTRA_ITTYPE] = "pvpAlterac:50"  }, -- Horn of the Frostwolf Howler
-				{ 3, 18245, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Horn of the Black War Wolf
-				{ 4, 18247, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Black War Kodo
-				{ 5, 18246, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Whistle of the Black War Raptor
-				{ 6, 18248, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Red Skeletal Warhorse
-				{ 7, 34129, [PRICE_EXTRA_ITTYPE] = PVP_MOUNT_PRICE }, -- Swift Warstrider
+				{ 1, 19029  }, -- Horn of the Frostwolf Howler
+				{ 3, 29469 }, -- Horn of the Black War Wolf
+				{ 4, 29466 }, -- Black War Kodo
+				{ 5, 29472 }, -- Whistle of the Black War Raptor
+				{ 6, 29470 }, -- Red Skeletal Warhorse
+				{ 7, 34129 }, -- Swift Warstrider
 				{ 16,  30609 }, -- Swift Nether Drake
 				{ 17,  37676 }, -- Vengeful Nether Drake
 				{ 18,  34092 }, -- Merciless Nether Drake
