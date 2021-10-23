3e38f32978c24c61d5e97e8b29df573c3a889ac5
Tenies <85525377+Tenies@users.noreply.github.com>
2021-10-23 06:30:52 +0300

    Hallow's End additions

--- AtlasLootClassic_Collections/data.lua
+++ AtlasLootClassic_Collections/data.lua
@@ -1151,141 +1151,6 @@ data["Valentineday"] = {
 	},
 }
 
-data["Halloween"] = {
-	name = AL["Hallow's End"],
-	ContentType = WORLD_EVENT_CONTENT,
-	LoadDifficulty = NORMAL_DIFF,
-	TableType = NORMAL_ITTYPE,
-	gameVersion = 1,
-	items = {
-		{ -- Halloween1
-			name = AL["Hallow's End"].." - "..AL["Misc"],
-			[NORMAL_DIFF] = {
-				{ 1,  20400 }, -- Pumpkin Bag
-				{ 3,  18633 }, -- Styleen's Sour Suckerpop
-				{ 4,  18632 }, -- Moonbrook Riot Taffy
-				{ 5,  18635 }, -- Bellara's Nutterbar
-				{ 6,  20557 }, -- Hallow's End Pumpkin Treat
-				{ 8,  20389 }, -- Candy Corn
-				{ 9,  20388 }, -- Lollipop
-				{ 10, 20390 }, -- Candy Bar
-			},
-		},
-		{ -- Halloween1
-			name = AL["Hallow's End"].." - "..AL["Wands"],
-			[NORMAL_DIFF] = {
-				{ 1, 20410 }, -- Hallowed Wand - Bat
-				{ 2, 20409 }, -- Hallowed Wand - Ghost
-				{ 3, 20399 }, -- Hallowed Wand - Leper Gnome
-				{ 4, 20398 }, -- Hallowed Wand - Ninja
-				{ 5, 20397 }, -- Hallowed Wand - Pirate
-				{ 6, 20413 }, -- Hallowed Wand - Random
-				{ 7, 20411 }, -- Hallowed Wand - Skeleton
-				{ 8, 20414 }, -- Hallowed Wand - Wisp
-			},
-		},
-		{ -- Halloween3
-			name = AL["Hallow's End"].." - "..AL["Masks"],
-			[NORMAL_DIFF] = {
-				{ 1,  20561 }, -- Flimsy Male Dwarf Mask
-				{ 2,  20391 }, -- Flimsy Male Gnome Mask
-				{ 3,  20566 }, -- Flimsy Male Human Mask
-				{ 4,  20564 }, -- Flimsy Male Nightelf Mask
-				{ 5,  20570 }, -- Flimsy Male Orc Mask
-				{ 6,  20572 }, -- Flimsy Male Tauren Mask
-				{ 7,  20568 }, -- Flimsy Male Troll Mask
-				{ 8,  20573 }, -- Flimsy Male Undead Mask
-				{ 16, 20562 }, -- Flimsy Female Dwarf Mask
-				{ 17, 20392 }, -- Flimsy Female Gnome Mask
-				{ 18, 20565 }, -- Flimsy Female Human Mask
-				{ 19, 20563 }, -- Flimsy Female Nightelf Mask
-				{ 20, 20569 }, -- Flimsy Female Orc Mask
-				{ 21, 20571 }, -- Flimsy Female Tauren Mask
-				{ 22, 20567 }, -- Flimsy Female Troll Mask
-				{ 23, 20574 }, -- Flimsy Female Undead Mask
-			},
-		},
-	},
-}
-
-data["HalloweenTBC"] = {
-	name = AL["Hallow's End"],
-	ContentType = WORLD_EVENT_CONTENT,
-	LoadDifficulty = NORMAL_DIFF,
-	TableType = NORMAL_ITTYPE,
-	gameVersion = 2,
-	items = {
-		{ -- Halloween1
-			name = AL["Hallow's End"].." - "..AL["Misc"],
-			[NORMAL_DIFF] = {
-				{ 1,  20400 }, -- Pumpkin Bag
-				{ 3,  18633 }, -- Styleen's Sour Suckerpop
-				{ 4,  18632 }, -- Moonbrook Riot Taffy
-				{ 5,  18635 }, -- Bellara's Nutterbar
-				{ 6,  20557 }, -- Hallow's End Pumpkin Treat
-				{ 8,  20389 }, -- Candy Corn
-				{ 9,  20388 }, -- Lollipop
-				{ 10, 20390 }, -- Candy Bar
-			},
-		},
-		{ -- Halloween1
-			name = AL["Hallow's End"].." - "..AL["Wands"],
-			[NORMAL_DIFF] = {
-				{ 1, 20410 }, -- Hallowed Wand - Bat
-				{ 2, 20409 }, -- Hallowed Wand - Ghost
-				{ 3, 20399 }, -- Hallowed Wand - Leper Gnome
-				{ 4, 20398 }, -- Hallowed Wand - Ninja
-				{ 5, 20397 }, -- Hallowed Wand - Pirate
-				{ 6, 20413 }, -- Hallowed Wand - Random
-				{ 7, 20411 }, -- Hallowed Wand - Skeleton
-				{ 8, 20414 }, -- Hallowed Wand - Wisp
-			},
-		},
-		{ -- Halloween3
-			name = AL["Hallow's End"].." - "..AL["Masks"],
-			[NORMAL_DIFF] = {
-				{ 1,  20561 }, -- Flimsy Male Dwarf Mask
-				{ 2,  20391 }, -- Flimsy Male Gnome Mask
-				{ 3,  20566 }, -- Flimsy Male Human Mask
-				{ 4,  20564 }, -- Flimsy Male Nightelf Mask
-				{ 5,  20570 }, -- Flimsy Male Orc Mask
-				{ 6,  20572 }, -- Flimsy Male Tauren Mask
-				{ 7,  20568 }, -- Flimsy Male Troll Mask
-				{ 8,  20573 }, -- Flimsy Male Undead Mask
-				{ 16, 20562 }, -- Flimsy Female Dwarf Mask
-				{ 17, 20392 }, -- Flimsy Female Gnome Mask
-				{ 18, 20565 }, -- Flimsy Female Human Mask
-				{ 19, 20563 }, -- Flimsy Female Nightelf Mask
-				{ 20, 20569 }, -- Flimsy Female Orc Mask
-				{ 21, 20571 }, -- Flimsy Female Tauren Mask
-				{ 22, 20567 }, -- Flimsy Female Troll Mask
-				{ 23, 20574 }, -- Flimsy Female Undead Mask
-			},
-		},
---@version-bcc@
-		{ -- SMHeadlessHorseman
-			name = C_Map_GetAreaInfo(796).." - "..AL["Graveyard - Headless Horseman"],
-			[NORMAL_DIFF] = {
-                { 1, 34075 }, -- Ring of Ghoulish Delight
-                { 2, 34073 }, -- The Horseman's Signet Ring
-                { 3, 34074 }, -- Witches Band
-                { 5, 33808 }, -- The Horseman's Helm
-                { 6, 38175 }, -- The Horseman's Blade
-                { 8, 33292 }, -- Hallowed Helm
-                { 10, 34068 }, -- Weighted Jack-o'-Lantern
-                { 12, 33277 }, -- Tome of Thomas Thomson
-                { 16, 37012 }, -- The Horseman's Reins
-                { 18, 33182 }, -- Swift Flying Broom        280% flying
-                { 19, 33176 }, -- Flying Broom              60% flying
-                { 21, 33184 }, -- Swift Magic Broom         100% ground
-                { 22, 37011 }, -- Magic Broom               60% ground
-                { 24, 33154 }, -- Sinister Squashling
-			},
-		},
---@end-version-bcc@
-	},
-}
-
 data["WinterVeil"] = {
 	name = AL["Feast of Winter Veil"],
 	ContentType = WORLD_EVENT_CONTENT,
@@ -1655,6 +1520,146 @@ data["Brewfest"] = {
 				{ 17,  37828 }, -- Great Brewfest Kodo
 				{ 19,  37863 }, -- Direbrew's Remote
 				{ 21,  38280 }, -- Direbrew's Dire Brew
+			},
+		},
+	},
+}
+
+data["Halloween"] = {
+	name = AL["Hallow's End"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	gameVersion = 1,
+	items = {
+		{ -- Halloween1
+			name = AL["Hallow's End"].." - "..AL["Misc"],
+			[NORMAL_DIFF] = {
+				{ 1,  20400 }, -- Pumpkin Bag
+				{ 3,  18633 }, -- Styleen's Sour Suckerpop
+				{ 4,  18632 }, -- Moonbrook Riot Taffy
+				{ 5,  18635 }, -- Bellara's Nutterbar
+				{ 6,  20557 }, -- Hallow's End Pumpkin Treat
+				{ 8,  20389 }, -- Candy Corn
+				{ 9,  20388 }, -- Lollipop
+				{ 10, 20390 }, -- Candy Bar
+				{ 12, 20516 }, -- Bobbing Apple
+			},
+		},
+		{ -- Halloween2
+			name = AL["Hallow's End"].." - "..AL["Wands"],
+			[NORMAL_DIFF] = {
+				{ 1, 20410 }, -- Hallowed Wand - Bat
+				{ 2, 20409 }, -- Hallowed Wand - Ghost
+				{ 3, 20399 }, -- Hallowed Wand - Leper Gnome
+				{ 4, 20398 }, -- Hallowed Wand - Ninja
+				{ 5, 20397 }, -- Hallowed Wand - Pirate
+				{ 6, 20413 }, -- Hallowed Wand - Random
+				{ 7, 20411 }, -- Hallowed Wand - Skeleton
+				{ 8, 20414 }, -- Hallowed Wand - Wisp
+			},
+		},
+		{ -- Halloween3
+			name = AL["Hallow's End"].." - "..AL["Masks"],
+			[NORMAL_DIFF] = {
+				{ 1,  20561 }, -- Flimsy Male Dwarf Mask
+				{ 2,  20391 }, -- Flimsy Male Gnome Mask
+				{ 3,  20566 }, -- Flimsy Male Human Mask
+				{ 4,  20564 }, -- Flimsy Male Nightelf Mask
+				{ 5,  20570 }, -- Flimsy Male Orc Mask
+				{ 6,  20572 }, -- Flimsy Male Tauren Mask
+				{ 7,  20568 }, -- Flimsy Male Troll Mask
+				{ 8,  20573 }, -- Flimsy Male Undead Mask
+				{ 16, 20562 }, -- Flimsy Female Dwarf Mask
+				{ 17, 20392 }, -- Flimsy Female Gnome Mask
+				{ 18, 20565 }, -- Flimsy Female Human Mask
+				{ 19, 20563 }, -- Flimsy Female Nightelf Mask
+				{ 20, 20569 }, -- Flimsy Female Orc Mask
+				{ 21, 20571 }, -- Flimsy Female Tauren Mask
+				{ 22, 20567 }, -- Flimsy Female Troll Mask
+				{ 23, 20574 }, -- Flimsy Female Undead Mask
+			},
+		},
+	},
+}
+
+data["HalloweenTBC"] = {
+	name = AL["Hallow's End"],
+	ContentType = WORLD_EVENT_CONTENT,
+	LoadDifficulty = NORMAL_DIFF,
+	TableType = NORMAL_ITTYPE,
+	gameVersion = 2,
+	items = {
+		{ -- Halloween1
+			name = AL["Hallow's End"].." - "..AL["Misc"],
+			[NORMAL_DIFF] = {
+				{ 1,  20400 }, -- Pumpkin Bag
+				{ 2,  33117 }, -- Jack-o'-Lantern
+				{ 4,  18633 }, -- Styleen's Sour Suckerpop
+				{ 5,  18632 }, -- Moonbrook Riot Taffy
+				{ 6,  18635 }, -- Bellara's Nutterbar
+				{ 8,  20389 }, -- Candy Corn
+				{ 9,  20388 }, -- Lollipop
+				{ 10, 20390 }, -- Candy Bar
+				{ 12, 20516 }, -- Bobbing Apple
+				{ 16, 37585 }, -- Chewy Fel Taffy
+				{ 17, 37583 }, -- G.N.E.R.D.S.
+				{ 18, 37582 }, -- Pyroblast Cinnamon Ball
+				{ 19, 37584 }, -- Soothing Spearmint Candy
+				{ 21,  20557 }, -- Hallow's End Pumpkin Treat
+				{ 22, 34068 }, -- Weighted Jack-o'-Lantern
+				{ 23,  37604 }, -- Tooth Pick
+				{ 25,  37606 }, -- Penny Pouch
+				{ 27,  33189 }, -- Rickety Magic Broom
+			},
+		},
+		{ -- Halloween2
+			name = AL["Hallow's End"].." - "..AL["Wands"],
+			[NORMAL_DIFF] = {
+				{ 1, 20410 }, -- Hallowed Wand - Bat
+				{ 2, 20409 }, -- Hallowed Wand - Ghost
+				{ 3, 20399 }, -- Hallowed Wand - Leper Gnome
+				{ 4, 20398 }, -- Hallowed Wand - Ninja
+				{ 5, 20397 }, -- Hallowed Wand - Pirate
+				{ 6, 20413 }, -- Hallowed Wand - Random
+				{ 7, 20411 }, -- Hallowed Wand - Skeleton
+				{ 8, 20414 }, -- Hallowed Wand - Wisp
+			},
+		},
+		{ -- Halloween3
+			name = AL["Hallow's End"].." - "..AL["Masks"],
+			[NORMAL_DIFF] = {
+				{ 1,  20561 }, -- Flimsy Male Dwarf Mask
+				{ 2,  20391 }, -- Flimsy Male Gnome Mask
+				{ 3,  20566 }, -- Flimsy Male Human Mask
+				{ 4,  20564 }, -- Flimsy Male Nightelf Mask
+				{ 5,  20570 }, -- Flimsy Male Orc Mask
+				{ 6,  20572 }, -- Flimsy Male Tauren Mask
+				{ 7,  20568 }, -- Flimsy Male Troll Mask
+				{ 8,  20573 }, -- Flimsy Male Undead Mask
+				{ 16, 20562 }, -- Flimsy Female Dwarf Mask
+				{ 17, 20392 }, -- Flimsy Female Gnome Mask
+				{ 18, 20565 }, -- Flimsy Female Human Mask
+				{ 19, 20563 }, -- Flimsy Female Nightelf Mask
+				{ 20, 20569 }, -- Flimsy Female Orc Mask
+				{ 21, 20571 }, -- Flimsy Female Tauren Mask
+				{ 22, 20567 }, -- Flimsy Female Troll Mask
+				{ 23, 20574 }, -- Flimsy Female Undead Mask
+			},
+		},
+		{ -- SMHeadlessHorseman
+			name = C_Map_GetAreaInfo(796).." - "..AL["Graveyard - Headless Horseman"],
+			[NORMAL_DIFF] = {
+                { 1, 34075 }, -- Ring of Ghoulish Delight
+                { 2, 34073 }, -- The Horseman's Signet Ring
+                { 3, 34074 }, -- Witches Band
+                { 5, 33808 }, -- The Horseman's Helm
+                { 6, 38175 }, -- The Horseman's Blade
+                { 8, 33292 }, -- Hallowed Helm
+                { 10, 34068 }, -- Weighted Jack-o'-Lantern
+                { 16, 37012 }, -- The Horseman's Reins
+                { 18, 37011 }, -- Magic Broom
+                { 20, 33154 }, -- Sinister Squashling
 			},
 		},
 	},
