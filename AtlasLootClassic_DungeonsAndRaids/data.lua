c9dfca3aa7e4a3423a26c50f7553e4e64d4e041a
Barov2 <113269863+Barov2@users.noreply.github.com>
2022-09-18 00:30:24 +0200

    Update data.lua

--- AtlasLootClassic_DungeonsAndRaids/data.lua
+++ AtlasLootClassic_DungeonsAndRaids/data.lua
@@ -1317,7 +1317,7 @@ data["ScarletMonasteryGraveyard"] = {
 				{ 2, 49123 }, -- The Horseman's Seal
 				{ 3, 49124 }, -- Wicked Witch's Band
 				{ 5, 49126 }, -- The Horseman's Horrific Helm
-				{ 6, 49128 }, -- The Horseman's Baleful Blad
+				{ 6, 49128 }, -- The Horseman's Baleful Blade
 				{ 8, 33292 }, -- Hallowed Helm
 				{ 10, 34068 }, -- Weighted Jack-o'-Lantern
 				{ 12, 33277 }, -- Tome of Thomas Thomson
