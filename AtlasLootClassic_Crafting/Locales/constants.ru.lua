8708c37a76755cd8743cba7fa2c70f35a4cfa1be
Mard35 <61734903+Mard35@users.noreply.github.com>
2022-11-27 10:01:32 +0300

    For Expertise food section Ru translation fix
    new constants for Expertise and Feast

--- AtlasLootClassic_Crafting/Locales/constants.ru.lua
+++ AtlasLootClassic_Crafting/Locales/constants.ru.lua
@@ -4,3 +4,5 @@ if not AL then return end
 
 -- These localization strings are translated on Curseforge: https://www.curseforge.com/wow/addons/atlaslootclassic/localization
 --@localization(locale="ruRU", format="lua_additive_table", table-name="AL", handle-unlocalized="ignore", namespace="Crafting")@
+AL["Expertise"] = "Мастерство"
+AL["Feast"] = "Пир"
