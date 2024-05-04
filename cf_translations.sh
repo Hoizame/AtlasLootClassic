9dcaab0f4833cc57ad4aaf9ff05d7a900e259da0
Snowflame <2276311+snowflame0@users.noreply.github.com>
2024-05-04 08:43:47 -0700

    Update cf_translations.sh

--- cf_translations.sh
+++ cf_translations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Source from: https://github.com/WeakAuras/WeakAuras2
 
-declare -A LOC_FILES=(
+declare -A locale_files=(
   ["Base Namespace"]="Global.lua"
   ["Collections"]="Collections.lua"
   ["Crafting"]="Crafting.lua"
@@ -23,7 +23,7 @@ do_import() {
   : > "$tempfile"
 
   echo -n "Importing $namespace..."
-  result=$( curl -sS -X POST -w "%{http_code}" -o "$tempfile" \
+  result=$( curl -sS -0 -X POST -w "%{http_code}" -o "$tempfile" \
     -H "X-Api-Token: $CF_API_KEY" \
     -F "metadata={ language: \"enUS\", namespace: \"$namespace\", \"missing-phrase-handling\": \"DeletePhrase\" }" \
     -F "localizations=<$file" \
@@ -33,7 +33,7 @@ do_import() {
     200) echo "done." ;;
     *)
       echo "error! ($result)"
-      [ -s "$tempfile" ] && grep -q "errorMessage" "$tempfile" && cat "$tempfile" | jq --raw-output '.errorMessage'
+      [ -s "$tempfile" ] && grep -q "errorMessage" "$tempfile" | jq --raw-output '.errorMessage' "$tempfile"
       exit 1
       ;;
   esac
@@ -42,8 +42,8 @@ do_import() {
 lua babelfish.lua || exit 1
 echo
 
-for namespace in "${!LOC_FILES[@]}"; do
-  do_import "$namespace" "${LOC_FILES[$namespace]}"
+for namespace in "${!locale_files[@]}"; do
+  do_import "$namespace" "${locale_files[$namespace]}"
 done
 
 exit 0
