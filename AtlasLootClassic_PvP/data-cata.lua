310bd95e797987a8c0c73413885aa7985adaf1bf
Snowflame <hectorm204@gmail.com>
2024-06-01 05:44:38 -0700

    added non-set bloodthirsty gladiator items

--- AtlasLootClassic_PvP/data-cata.lua
+++ AtlasLootClassic_PvP/data-cata.lua
@@ -36,7 +36,7 @@ else
 end
 local SET1_DIFF = data:AddDifficulty(format(AL["Bloodthirsty %s"], ""), "set1", nil, 1)
 local SET2_DIFF = data:AddDifficulty(format(AL["Vicious %s"], ""), "set2", nil, 1)
---local SET3_DIFF = data:AddDifficulty(format(AL["Vicious %s"], ""), "set3", nil, 1)
+local SET3_DIFF = data:AddDifficulty(format(AL["Elite Vicious %s"], ""), "set3", nil, 1)
 
 local NORMAL_ITTYPE = data:AddItemTableType("Item", "Item")
 local SET_ITTYPE = data:AddItemTableType("Set", "Item")
@@ -84,77 +84,206 @@ data["ArenaS9PvP"] = {
     }, {
         name = AL["Weapons"] .. " - " .. AL["One-Handed"],
         [SET1_DIFF] = {
+            { 1, 64717 }, -- Bloodthirsty Gladiator's Cleaver
+            { 2, 64758 }, -- Bloodthirsty Gladiator's Hacker
+            { 4, 64700 }, -- Bloodthirsty Gladiator's Bonecracker
+            { 5, 64816 }, -- Bloodthirsty Gladiator's Pummeler
+            { 7, 64817 }, -- Bloodthirsty Gladiator's Quickblade
+            { 8, 64858 }, -- Bloodthirsty Gladiator's Slicer
+            { 16, 64859 }, -- Bloodthirsty Gladiator's Spellblade
+            { 17, 64752 }, -- Bloodthirsty Gladiator's Gavel
+            { 19, 64848 }, -- Bloodthirsty Gladiator's Shanker
+            { 20, 64850 }, -- Bloodthirsty Gladiator's Shiv
+            { 22, 64825 }, -- Bloodthirsty Gladiator's Right Render
+            { 23, 64826 }, -- Bloodthirsty Gladiator's Right Ripper
+            { 25, 64774 }, -- Bloodthirsty Gladiator's Left Render
+            { 26, 64775 }, -- Bloodthirsty Gladiator's Left Ripper
         },
         [SET2_DIFF] = {
         },
     }, {
         name = AL["Weapons"] .. " - " .. AL["Two-Handed"],
         [SET1_DIFF] = {
+            { 1, 64810 }, -- Bloodthirsty Gladiator's Pike
+            { 2, 64860 }, -- Bloodthirsty Gladiator's Staff
+            { 4, 64726 }, -- Bloodthirsty Gladiator's Decapitator
+            { 5, 64701 }, -- Bloodthirsty Gladiator's Bonegrinder
+            { 6, 64755 }, -- Bloodthirsty Gladiator's Greatsword
+            { 16, 64695 }, -- Bloodthirsty Gladiator's Battle Staff
+            { 17, 64744 }, -- Bloodthirsty Gladiator's Energy Staff
         },
         [SET2_DIFF] = {
         },
     }, {
         name = AL["Weapons"] .. " - " .. AL["Ranged"],
         [SET1_DIFF] = {
+            { 1, 64783 }, -- Bloodthirsty Gladiator's Longbow
+            { 2, 64760 }, -- Bloodthirsty Gladiator's Heavy Crossbow
+            { 3, 64824 }, -- Bloodthirsty Gladiator's Rifle
+            { 5, 64759 }, -- Bloodthirsty Gladiator's Hatchet
+            { 6, 64871 }, -- Bloodthirsty Gladiator's War Edge
+            { 16, 64694 }, -- Bloodthirsty Gladiator's Baton of Light
+            { 17, 64861 }, -- Bloodthirsty Gladiator's Touch of Defeat
         },
         [SET2_DIFF] = {
         },
     }, {
         name = AL["Weapons"] .. " - " .. ALIL["Off Hand"],
         [SET1_DIFF] = {
+            { 1, 64743 }, -- Bloodthirsty Gladiator's Endgame
+            { 2, 64823 }, -- Bloodthirsty Gladiator's Reprieve
         },
         [SET2_DIFF] = {
         },
     }, {
         name = AL["Weapons"] .. " - " .. ALIL["Shields"],
         [SET1_DIFF] = {
+            { 1, 64693 }, -- Bloodthirsty Gladiator's Barrier
+            { 2, 64818 }, -- Bloodthirsty Gladiator's Redoubt
+            { 3, 64849 }, -- Bloodthirsty Gladiator's Shield Wall
         },
         [SET2_DIFF] = {
         },
     }, {
         name = ALIL["Cloak"],
         [SET1_DIFF] = {
+            { 1, 64706 }, -- Bloodthirsty Gladiator's Cape of Cruelty
+            { 2, 64707 }, -- Bloodthirsty Gladiator's Cape of Prowess
+            { 4, 64718 }, -- Bloodthirsty Gladiator's Cloak of Alacrity
+            { 5, 64719 }, -- Bloodthirsty Gladiator's Cloak of Prowess
+            { 16, 64732 }, -- Bloodthirsty Gladiator's Drape of Diffusion
+            { 17, 64733 }, -- Bloodthirsty Gladiator's Drape of Meditation
+            { 18, 64734 }, -- Bloodthirsty Gladiator's Drape of Prowess
         },
         [SET2_DIFF] = {
         },
     }, {
         name = ALIL["Relic"],
         [SET1_DIFF] = {
+            { 1, 64819 }, -- Bloodthirsty Gladiator's Relic of Conquest
+            { 2, 64820 }, -- Bloodthirsty Gladiator's Relic of Dominance
+            { 3, 64821 }, -- Bloodthirsty Gladiator's Relic of Salvation
+            { 4, 64822 }, -- Bloodthirsty Gladiator's Relic of Triumph
         },
         [SET2_DIFF] = {
         },
     }, {
         name = ALIL["Neck"],
         [SET1_DIFF] = {
+            { 1, 64713 }, -- Bloodthirsty Gladiator's Choker of Accuracy
+            { 2, 64714 }, -- Bloodthirsty Gladiator's Choker of Proficiency
+            { 5, 64800 }, -- Bloodthirsty Gladiator's Necklace of Proficiency
+            { 6, 64801 }, -- Bloodthirsty Gladiator's Necklace of Prowess
+            { 16, 64807 }, -- Bloodthirsty Gladiator's Pendant of Alacrity
+            { 17, 64808 }, -- Bloodthirsty Gladiator's Pendant of Diffusion
+            { 18, 64809 }, -- Bloodthirsty Gladiator's Pendant of Meditation
         },
         [SET2_DIFF] = {
         },
     }, {
         name = ALIL["Finger"],
         [SET1_DIFF] = {
+            { 1, 64851 }, -- Bloodthirsty Gladiator's Signet of Accuracy
+            { 2, 64852 }, -- Bloodthirsty Gladiator's Signet of Cruelty
+            { 4, 64832 }, -- Bloodthirsty Gladiator's Ring of Accuracy
+            { 5, 64833 }, -- Bloodthirsty Gladiator's Ring of Cruelty
+            { 16, 64690 }, -- Bloodthirsty Gladiator's Band of Accuracy
+            { 17, 64691 }, -- Bloodthirsty Gladiator's Band of Cruelty
+            { 18, 64692 }, -- Bloodthirsty Gladiator's Band of Meditation
+
+
         },
         [SET2_DIFF] = {
         },
     }, {
         name = format(AL["Non Set '%s'"], ALIL["Cloth"]),
         [NORMAL_DIFF] = {
+            { 1, 64862 }, -- Bloodthirsty Gladiator's Treads of Alacrity
+            { 2, 64863 }, -- Bloodthirsty Gladiator's Treads of Cruelty
+            { 3, 64864 }, -- Bloodthirsty Gladiator's Treads of Meditation
+            { 5, 64720 }, -- Bloodthirsty Gladiator's Cord of Accuracy
+            { 6, 64721 }, -- Bloodthirsty Gladiator's Cord of Cruelty
+            { 7, 64722 }, -- Bloodthirsty Gladiator's Cord of Meditation
+            { 16, 64723 }, -- Bloodthirsty Gladiator's Cuffs of Accuracy
+            { 17, 64724 }, -- Bloodthirsty Gladiator's Cuffs of Meditation
+            { 18, 64725 }, -- Bloodthirsty Gladiator's Cuffs of Prowess
         }
     }, {
         name = format(AL["Non Set '%s'"], ALIL["Leather"]),
         [NORMAL_DIFF] = {
+            { 1, 64702 }, -- Bloodthirsty Gladiator's Boots of Alacrity
+            { 2, 64703 }, -- Bloodthirsty Gladiator's Boots of Cruelty
+            { 3, 64750 }, -- Bloodthirsty Gladiator's Footguards of Alacrity
+            { 4, 64751 }, -- Bloodthirsty Gladiator's Footguards of Meditation
+            { 6, 64696 }, -- Bloodthirsty Gladiator's Belt of Cruelty
+            { 7, 64697 }, -- Bloodthirsty Gladiator's Belt of Meditation
+            { 8, 64865 }, -- Bloodthirsty Gladiator's Waistband of Accuracy
+            { 9, 64866 }, -- Bloodthirsty Gladiator's Waistband of Cruelty
+            { 16, 64685 }, -- Bloodthirsty Gladiator's Armwraps of Accuracy
+            { 17, 64686 }, -- Bloodthirsty Gladiator's Armwraps of Alacrity
+            { 18, 64698 }, -- Bloodthirsty Gladiator's Bindings of Meditation
+            { 19, 64699 }, -- Bloodthirsty Gladiator's Bindings of Prowess
         }
     }, {
         name = format(AL["Non Set '%s'"], ALIL["Mail"]),
         [NORMAL_DIFF] = {
+            { 1, 64835 }, -- Bloodthirsty Gladiator's Sabatons of Alacrity
+            { 2, 64834 }, -- Bloodthirsty Gladiator's Sabatons of Alacrity
+            { 3, 64836 }, -- Bloodthirsty Gladiator's Sabatons of Cruelty
+            { 4, 64837 }, -- Bloodthirsty Gladiator's Sabatons of Meditation
+            { 6, 64781 }, -- Bloodthirsty Gladiator's Links of Accuracy
+            { 7, 64782 }, -- Bloodthirsty Gladiator's Links of Cruelty
+            { 8, 64867 }, -- Bloodthirsty Gladiator's Waistguard of Cruelty
+            { 9, 64868 }, -- Bloodthirsty Gladiator's Waistguard of Meditation
+            { 16, 64681 }, -- Bloodthirsty Gladiator's Armbands of Meditation
+            { 17, 64682 }, -- Bloodthirsty Gladiator's Armbands of Prowess
+            { 18, 64872 }, -- Bloodthirsty Gladiator's Wristguards of Accuracy
+            { 19, 64873 }, -- Bloodthirsty Gladiator's Wristguards of Alacrity
         }
     }, {
         name = format(AL["Non Set '%s'"], ALIL["Plate"]),
         [NORMAL_DIFF] = {
+            { 1, 64756 }, -- Bloodthirsty Gladiator's Greaves of Alacrity
+            { 2, 64757 }, -- Bloodthirsty Gladiator's Greaves of Meditation
+            { 3, 64869 }, -- Bloodthirsty Gladiator's Warboots of Alacrity
+            { 4, 64870 }, -- Bloodthirsty Gladiator's Warboots of Cruelty
+            { 6, 64715 }, -- Bloodthirsty Gladiator's Clasp of Cruelty
+            { 7, 64716 }, -- Bloodthirsty Gladiator's Clasp of Meditation
+            { 8, 64753 }, -- Bloodthirsty Gladiator's Girdle of Cruelty
+            { 9, 64754 }, -- Bloodthirsty Gladiator's Girdle of Prowess
+            { 16, 64683 }, -- Bloodthirsty Gladiator's Armplates of Alacrity
+            { 17, 64684 }, -- Bloodthirsty Gladiator's Armplates of Proficiency
+            { 18, 64704 }, -- Bloodthirsty Gladiator's Bracers of Meditation
+            { 19, 64705 }, -- Bloodthirsty Gladiator's Bracers of Prowess
+        }
+    }, {
+        name = ALIL["Trinket"],
+        [SET1_DIFF] = {
+            { 1, 64687 }, -- Bloodthirsty Gladiator's Badge of Conquest
+            { 2, 64688 }, -- Bloodthirsty Gladiator's Badge of Dominance
+            { 3, 64689 }, -- Bloodthirsty Gladiator's Badge of Victory
+            { 5, 64740 }, -- Bloodthirsty Gladiator's Emblem of Cruelty
+            { 6, 64741 }, -- Bloodthirsty Gladiator's Emblem of Meditation
+            { 7, 64742 }, -- Bloodthirsty Gladiator's Emblem of Tenacity
+            { 9, 64761 }, -- Bloodthirsty Gladiator's Insignia of Conquest
+            { 10, 64762 }, -- Bloodthirsty Gladiator's Insignia of Dominance
+            { 11, 64763 }, -- Bloodthirsty Gladiator's Insignia of Victory
+            { 16, 69787 }, -- Bloodthirsty Gladiator's Mark of Cruelty
+            { 17, 69789 }, -- Bloodthirsty Gladiator's Mark of Meditation
+            { 18, 69788 }, -- Bloodthirsty Gladiator's Mark of Tenacity
+            { 20, AtlasLoot:GetRetByFaction(64789, 64790) }, -- Bloodthirsty Gladiator's Medallion of Cruelty
+            { 21, AtlasLoot:GetRetByFaction(64792, 64791) }, -- Bloodthirsty Gladiator's Medallion of Meditation
+            { 22, AtlasLoot:GetRetByFaction(64794, 64793) }, -- Bloodthirsty Gladiator's Medallion of Tenacity
+            { 24, 69790 }, -- Bloodthirsty Gladiator's Symbol of Cruelty
+            { 25, 69792 }, -- Bloodthirsty Gladiator's Symbol of Meditation
+            { 26, 69791 }, -- Bloodthirsty Gladiator's Symbol of Tenacity
         }
     }, {
         name = AL["Gladiator Mount"],
         ExtraList = true,
         [NORMAL_DIFF] = {
+            { 1, "ac6003" },
+            { 2, 71339 },
         }
     }}
 }
